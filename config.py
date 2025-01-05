392c6602e28d298efda8c1775c34018fc9647fb6
rohitclasses12345 <rohitclasses12345@gmail.com>
2025-01-06 03:14:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_NAME = "Luminatbot"
-API_ID    = 21567814
-API_HASH  = "cd7dc5431d449fd795683c550d7bfb7e"
-BOT_TOKEN = "7662813243:AAEA3yIU_BZTCHR-lEXRFBelPfDdubYuRFA"
+API_ID    = 20607064
+API_HASH  = "c0a09fd762681a66366cf84976f31a17"
+BOT_TOKEN = "7939928794:AAGTFVaaKrixxk2P3vuN60wz2_XSW9PcYy4"
 #MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
