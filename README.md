2fa54cbc97e9bd4a03c607faeb9bc34a42306272
rohitclasses12345 <rohitclasses12345@gmail.com>
2025-01-06 03:12:39 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/lakshaybajarang/Lakshay_Paid.git)
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/AirPheonixSaksham/test3.git)
---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_NAME = "Luminatbot"
+API_ID    = 21567814
+API_HASH  = "cd7dc5431d449fd795683c550d7bfb7e"
+BOT_TOKEN = "7662813243:AAEA3yIU_BZTCHR-lEXRFBelPfDdubYuRFA"
+#MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
+MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
